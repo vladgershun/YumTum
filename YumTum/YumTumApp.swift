4d33df076c1a54013e468ce84af7c58a6970c726
Vlad Gershun <dev.vladgershun@gmail.com>
2023-01-10 09:32:37 -0800

    Initial Commit

---
+++ YumTum/YumTumApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumTumApp.swift
+//  YumTum
+//
+//  Created by Vlad Gershun on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct YumTumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
